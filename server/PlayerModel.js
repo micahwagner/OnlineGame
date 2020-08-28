be63a851ea29b551cacfa7f4262420af109bb7d9
Micah Wagner <micah.wagner@icloud.com>
2020-08-27 19:21:09 -0700

    added players obj

---
+++ server/PlayerModel.js
@@ -0,0 +1,8 @@
+class PlayerModel{
+	constructor(playerID, pos){
+		this.playerID = playerID;
+		this.pos = pos;
+	}
+}
+
+module.exports = PlayerModel;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var server = require('http').createServer(express);
 var io = require('socket.io')(server);
+var PlayerModel = require("../server/PlayerModel.js");
+
+let players = {}
 
 io.on('connection', function(socket) {
-	console.log('a user connected ' + socket.id);
+	createPlayer(socket);
+	console.log(players);
 
 	socket.on('disconnect', function() {
 		console.log('user disconnected '+ socket.id);
 	});
 });
+
+function createPlayer(socket){
+	let player = new PlayerModel(socket.id, [0, 0]);
+	players[socket.id] = player.pos;
+}
 
 // app.use(express.static(__dirname + '/public'));
 
